8ebbc9eeae47aa108d1daa214da75c7049dd7406
SYNC3D <cobus.myburgh@synced.co.za>
2021-10-01 17:03:06 +0200

    V1 complete

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # wallhaven
+...
+My name is Cobus Myburgh and I am a "below" junior developer.
+I have been in Infratructure for over 20 years but this is my first dig at writing code.
+
+This is a simple script to download wallpapers from Wallhaven using Python
+
+Settings can be found here https://wallhaven.cc/help/api
+
+Please use your own API.
--- wallhaven.py
+++ wallhaven.py
@@ -1,41 +1,42 @@
 import requests
 import json
 import urllib3
-import wget
+import os
 
 http = urllib3.PoolManager()
 paths = []
+downloaddir = "~/Pictures/Downloaded/"
 
 parameters = {
     "categories": "101",
-    "purity": "010",
+    "purity": "100",
     "sorting": "random",
-    "resolutions": "1920x1080",
-    "ratio": "16x9"
+    "atleast": "1920x1080, 2560x1440",
+    "ratios": "16x9",
 }
+# This is where you have to insert your apikey. Not sure whether I should hardcode it load it
+# from somewhere else. I suspect it might be better to load it from somewhere else.
 
 response = requests.get(
     "https://wallhaven.cc/api/v1/search?apikey=SduhMlFOWiBPTgIOPpjKK9uq6w63noqV", params=parameters)
 
-
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
+# Calling the API and storing the output in a dictionary
 
 data = response.json()['data']
 for d in data:
     url = d['path']
     paths.append(url)
 
-print(paths)
+# Reading the Dictionary one item at a time and downloading it.
+
 for I in paths:
     url = I
     url2 = I
+    print(I)
     if url2.find('/'):
         url3 = (url2.rsplit('/', 1)[1])
+        completeName = os.path.join(downloaddir, url3)
     data = requests.get(url)
     # Save file data to local copy
-    with open(url3, 'wb')as file:
+    with open(completeName, 'wb')as file:
         file.write(data.content)
